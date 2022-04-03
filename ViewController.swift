4c733f380d8d7e3b411c50aec15d5616c1a37fae
WenJie Qiu <qiuwenjie1997@163.com>
2022-04-03 18:01:59 +0800

    完成了点击按钮播放相应🎵

--- ViewController.swift
+++ ViewController.swift
@@ -6,14 +6,32 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    var player: AVAudioPlayer?
+    var sounds = ["note1", "note2", "note3", "note4", "note5", "note6", "note7"]
+    
+    @IBAction func play(_ sender: Any) {
+        let btn = sender as! UIButton
+        do {
+        
+            let url = Bundle.main.url(forResource: sounds[btn.tag - 1], withExtension: "wav")
+            guard let url = url else { return }
+            player = try AVAudioPlayer(contentsOf: url)
+            player?.play()
+        } catch {
+            print(error)
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    
 
 }
 
